4a036be16bbe28509d8b20312242965abbd4623c
Colin Alworth <colinalworth@deephaven.io>
2022-06-03 14:16:25 -0500

    correctly serialize all partitionedtable types

--- plugin/partitionedtable/src/main/java/io/deephaven/partitionedtable/PartitionedTableTypePlugin.java
+++ plugin/partitionedtable/src/main/java/io/deephaven/partitionedtable/PartitionedTableTypePlugin.java
@@ -4,8 +4,10 @@ import com.google.auto.service.AutoService;
 import com.google.flatbuffers.FlatBufferBuilder;
 import com.google.protobuf.ByteString;
 import io.deephaven.engine.table.PartitionedTable;
+import io.deephaven.engine.table.impl.partitioned.PartitionedTableImpl;
 import io.deephaven.extensions.barrage.util.BarrageUtil;
 import io.deephaven.plugin.type.ObjectType;
+import io.deephaven.plugin.type.ObjectTypeBase;
 import io.deephaven.plugin.type.ObjectTypeClassBase;
 import io.deephaven.proto.backplane.grpc.PartitionedTableDescriptor;
 import io.deephaven.proto.flight.util.MessageHelper;
@@ -15,27 +17,35 @@ import java.io.OutputStream;
 import java.util.Collections;
 
 @AutoService(ObjectType.class)
-public class PartitionedTableTypePlugin extends ObjectTypeClassBase<PartitionedTable> {
-    public PartitionedTableTypePlugin(String name, Class<PartitionedTable> clazz) {
-        super(name, clazz);
+public class PartitionedTableTypePlugin extends ObjectTypeBase {
+
+    @Override
+    public String name() {
+        return "PartitionedTable";
     }
 
     @Override
-    public void writeToImpl(Exporter exporter, PartitionedTable object, OutputStream out) throws IOException {
-        exporter.reference(object, false, true);
+    public boolean isType(Object object) {
+        return object instanceof PartitionedTable;
+    }
+
+    @Override
+    public void writeCompatibleObjectTo(Exporter exporter, Object object, OutputStream out) throws IOException {
+        PartitionedTable partitionedTable = (PartitionedTable) object;
+        exporter.reference(partitionedTable.table(), false, true);
         // Send Schema wrapped in Message
         final FlatBufferBuilder builder = new FlatBufferBuilder();
-        final int schemaOffset = BarrageUtil.makeSchemaPayload(builder, object.constituentDefinition(),
+        final int schemaOffset = BarrageUtil.makeSchemaPayload(builder, partitionedTable.constituentDefinition(),
                 Collections.emptyMap());
         builder.finish(MessageHelper.wrapInMessage(builder, schemaOffset,
                 org.apache.arrow.flatbuf.MessageHeader.Schema));
 
         PartitionedTableDescriptor result = PartitionedTableDescriptor.newBuilder()
-                .addAllKeyColumnNames(object.keyColumnNames())
-                .setUniqueKeys(object.uniqueKeys())
+                .addAllKeyColumnNames(partitionedTable.keyColumnNames())
+                .setUniqueKeys(partitionedTable.uniqueKeys())
                 .setConstituentDefinitionSchema(ByteString.copyFrom(builder.dataBuffer()))
-                .setConstituentColumnName(object.constituentColumnName())
-                .setConstituentChangesPermitted(object.constituentChangesPermitted())
+                .setConstituentColumnName(partitionedTable.constituentColumnName())
+                .setConstituentChangesPermitted(partitionedTable.constituentChangesPermitted())
                 .build();
         result.writeTo(out);
     }
