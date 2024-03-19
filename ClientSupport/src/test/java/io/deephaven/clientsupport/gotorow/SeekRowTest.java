486dc39672349bcb905e57c2e44bf85ef220e931
Colin Alworth <colinalworth@deephaven.io>
2024-03-19 13:34:12 -0500

    Factor out more code to share between tests

--- ClientSupport/src/test/java/io/deephaven/clientsupport/gotorow/SeekRowTest.java
+++ ClientSupport/src/test/java/io/deephaven/clientsupport/gotorow/SeekRowTest.java
@@ -19,7 +19,18 @@ public class SeekRowTest {
     @Rule
     public final EngineCleanup framework = new EngineCleanup();
 
-    private static void assertSeekPosition(int seekValue, Table t, boolean seekForward, int currentPosition, int expectedPosition) {
+    /**
+     * Helper to run SeekRow and validate that the discovered row is what was expected. Validates the
+     * {@code expectedPosition} before running, to ensure test data makes sense.
+     *
+     * @param seekValue the value to search for
+     * @param t the table to search
+     * @param seekForward true to seek forward, false to seek backward
+     * @param currentPosition the position to start searching
+     * @param expectedPosition the next expected position of the seek value
+     */
+    private static void assertSeekPosition(int seekValue, Table t, boolean seekForward, int currentPosition,
+            int expectedPosition) {
         if (expectedPosition != -1) {
             // Confirm that the expected position matches
             assertEquals(seekValue, t.flatten().getColumnSource("num").getInt(expectedPosition));
@@ -34,8 +45,13 @@ public class SeekRowTest {
 
     @Test
     public void emptyTable() {
-        Table t = TableTools.newTable(intCol("num")).sort("num");
+        Table t = TableTools.newTable(intCol("num"));
 
+        assertSeekPosition(1, t, true, 0, -1);
+        assertSeekPosition(1, t, false, 0, -1);
+
+        // repeat with sorted
+        t = t.sort("num");
         assertSeekPosition(1, t, true, 0, -1);
         assertSeekPosition(1, t, false, 0, -1);
     }
@@ -64,7 +80,40 @@ public class SeekRowTest {
 
         assertSeekPosition(100, t, false, 0, -1);
         assertSeekPosition(100, t, true, 0, -1);
+    }
 
+    /**
+     * Helper to seek from every row in a table, and assert that a valid value can be found in a valid position from
+     * each.
+     * 
+     * @param seekValue the value to search for
+     * @param t the table to search
+     * @param forwardPosition expected positions when searching forward, indexed on starting row
+     * @param backwardPosition expected positions when searching backwards, indexed on starting row
+     */
+    private static void assertSeekPositionAllRows(int seekValue, Table t, int[] forwardPosition,
+            int[] backwardPosition) {
+        assertEquals(t.size(), forwardPosition.length);
+        assertEquals(t.size(), backwardPosition.length);
+        for (int i = 0; i < t.size(); i++) {
+            // seek from the current position, confirm we get the expected position
+            assertSeekPosition(seekValue, t, true, i, forwardPosition[i]);
+            assertSeekPosition(seekValue, t, false, i, backwardPosition[i]);
+        }
+    }
+
+    /**
+     * Helper to verify that a given value can not be found no matter which row is started from
+     * 
+     * @param impossibleValue a value that isn't present in the table
+     * @param t the table to search
+     */
+    public static void assertNotFound(int impossibleValue, Table t) {
+        // ensure we can't find values that don't exist
+        for (int i = 0; i < t.size(); i++) {
+            assertSeekPosition(impossibleValue, t, true, i, -1);
+            assertSeekPosition(impossibleValue, t, false, i, -1);
+        }
     }
 
     @Test
@@ -74,29 +123,13 @@ public class SeekRowTest {
         int seekValue = 3;
         int[] forwardPosition = {3, 3, 3, 4, 3, 3, 3};
         int[] backwardPosition = {4, 4, 4, 4, 3, 4, 4};
-        for (int i = 0; i < t.size(); i++) {
-            // seek from the current position, confirm we get the expected position
-            assertSeekPosition(seekValue, t, true, i, forwardPosition[i]);
-            assertSeekPosition(seekValue, t, false, i, backwardPosition[i]);
-        }
-        // ensure we can't find values that don't exist
-        for (int i = 0; i < t.size(); i++) {
-            assertSeekPosition(6, t, true, i, -1);
-            assertSeekPosition(6, t, false, i, -1);
-        }
+        assertSeekPositionAllRows(seekValue, t, forwardPosition, backwardPosition);
+        assertNotFound(6, t);
 
         // repeat, with table actually marked as sorted
         t = t.sort("num");
-        for (int i = 0; i < t.size(); i++) {
-            // seek from the current position, confirm we get the expected position
-            assertSeekPosition(seekValue, t, true, i, forwardPosition[i]);
-            assertSeekPosition(seekValue, t, false, i, backwardPosition[i]);
-        }
-        // ensure we can't find values that don't exist
-        for (int i = 0; i < t.size(); i++) {
-            assertSeekPosition(6, t, true, i, -1);
-            assertSeekPosition(6, t, false, i, -1);
-        }
+        assertSeekPositionAllRows(seekValue, t, forwardPosition, backwardPosition);
+        assertNotFound(6, t);
     }
 
     @Test
@@ -105,32 +138,16 @@ public class SeekRowTest {
         int seekValue = 3;
         int[] forwardPosition = {2, 2, 3, 2, 2, 2, 2};
         int[] backwardPosition = {3, 3, 3, 2, 3, 3, 3};
-        for (int i = 0; i < t.size(); i++) {
-            // seek from the current position, confirm we get the expected position
-            assertSeekPosition(seekValue, t, true, i, forwardPosition[i]);
-            assertSeekPosition(seekValue, t, false, i, backwardPosition[i]);
-        }
-        // ensure we can't find values that don't exist
-        for (int i = 0; i < t.size(); i++) {
-            assertSeekPosition(6, t, true, i, -1);
-            assertSeekPosition(6, t, false, i, -1);
-        }
+        assertSeekPositionAllRows(seekValue, t, forwardPosition, backwardPosition);
+        assertNotFound(6, t);
 
         t = t.sortDescending("num");
-        for (int i = 0; i < t.size(); i++) {
-            // seek from the current position, confirm we get the expected position
-            assertSeekPosition(seekValue, t, true, i, forwardPosition[i]);
-            assertSeekPosition(seekValue, t, false, i, backwardPosition[i]);
-        }
-        // ensure we can't find values that don't exist
-        for (int i = 0; i < t.size(); i++) {
-            assertSeekPosition(6, t, true, i, -1);
-            assertSeekPosition(6, t, false, i, -1);
-        }
+        assertSeekPositionAllRows(seekValue, t, forwardPosition, backwardPosition);
+        assertNotFound(6, t);
     }
 
     @Test
     public void unsortedContentSeek() {
-        //TODO
+        // TODO
     }
 }
