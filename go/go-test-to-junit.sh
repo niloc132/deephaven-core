fd7d9e24adf2a8a23397f240f4c923335a1e8361
Colin Alworth <colinalworth@deephaven.io>
2025-01-14 14:45:42 -0600

    Try pcap when go tests are running, hangup nicely, rm if tests pass

--- go/go-test-to-junit.sh
+++ go/go-test-to-junit.sh
@@ -6,4 +6,13 @@ set -o xtrace
 
 XML=${1};
 
-go test -vet=all -v ./... 2>&1 | go-junit-report -set-exit-code -iocopy -out "$XML";
+tcpdump -i any -w /out/tcpdump.pcap &
+go test -vet=all -v ./... 2>&1 | go-junit-report -set-exit-code -iocopy -out "$XML" && ret=$? || ret=$?;
+kill %1;
+if [ $ret -eq 0 ] ; then
+    echo "Tests passed, deleting pcap"
+    rm /out/tcpdump.pcap
+else
+    echo "Tests failed, emitting exit code $ret"
+    exit $ret
+fi
